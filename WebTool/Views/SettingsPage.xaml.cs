cdc32ae79da0c1c8990470d85f471d05ea017fa2
Andrés Balderas <balnorie@gmail.com>
2019-09-01 16:36:25 -0500

    Add settings page

---
+++ WebTool/Views/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WebTool.Views
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
