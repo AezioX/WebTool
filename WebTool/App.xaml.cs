45f50ffde00e32844942ebbdf8b5976a46c36767
Andrés Balderas <balnorie@gmail.com>
2019-09-01 15:33:30 -0500

    Add Shell

--- WebTool/App.xaml.cs
+++ WebTool/App.xaml.cs
@@ -10,7 +10,7 @@ namespace WebTool
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
