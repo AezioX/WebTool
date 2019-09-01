021b5a507a8b1154635bd76ae3292d6dd6c5e9ca
Andrés Balderas <balnorie@gmail.com>
2019-09-01 17:24:18 -0500

    Create change theme commands

--- WebTool/ViewModels/SettingsViewModel.cs
+++ WebTool/ViewModels/SettingsViewModel.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 namespace WebTool.ViewModels
 {
     public class SettingsViewModel : BaseViewModel
     {
+        public ICommand ChangeToAezioXThemeCommand => new Command(ChangeToAezioXTheme);
+
+        public ICommand ChangeToDefaultThemeCommand => new Command(ChangeToDefaultTheme);
+
         public SettingsViewModel()
         {
+        }
+
+        private void ChangeToAezioXTheme()
+        {
+            App.ChangeThemeToAezioxChoice();
+        }
+
+        private void ChangeToDefaultTheme()
+        {
+            App.ChangeThemeToDefaultTheme();
         }
     }
 }
