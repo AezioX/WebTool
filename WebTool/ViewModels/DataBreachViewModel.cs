6ddec6c3a5ea2904ced95fbb60587905056f4d5e
Andrés Balderas <balnorie@gmail.com>
2019-09-02 22:21:09 -0500

    Config base DataBreach ViewModel

--- WebTool/ViewModels/DataBreachViewModel.cs
+++ WebTool/ViewModels/DataBreachViewModel.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using WebTool.Models.DataBreach;
+
 namespace WebTool.ViewModels
 {
     public class DataBreachViewModel : BaseViewModel
     {
+        public ICommand CheckAccountCommand => new Command(CheckAccount);
+
         public DataBreachViewModel()
         {
+        }
+
+        private void CheckAccount()
+        {
+            
+        }
+
+        private ObservableCollection<BreachResults> _results;
+        public ObservableCollection<BreachResults> Results
+        {
+            get => _results;
+
+            set => SetProperty(ref _results, value);
+        }
+
+        private string _account;
+        public string Account
+        {
+            get => _account;
+
+            set => SetProperty(ref _account, value);
         }
     }
 }
