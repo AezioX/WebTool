ac6bd9173c03c97753e9c633c7b373e118701271
Andrés Balderas <balnorie@gmail.com>
2019-09-03 00:37:15 -0500

    Add Newtonsoft.Json

---
+++ WebTool/Models/DataBreach/jsonBreachResults.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebTool.Models.DataBreach
+{
+    public class jsonBreachResults
+    {
+        public jsonBreachResults()
+        {
+        }
+    }
+}
