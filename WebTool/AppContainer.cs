dcc040ed185b6a8b1fccaa420a971040016aa527
Andrés Balderas <balnorie@gmail.com>
2019-09-04 20:50:06 -0500

    Create AppContainer for Dependency Injection

--- WebTool/AppContainer.cs
+++ WebTool/AppContainer.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using Autofac;
+using WebTool.Views;
+using WebTool.ViewModels;
+using WebTool.Services.DataBreach;
+
 namespace WebTool
 {
     public class AppContainer
     {
-        public AppContainer()
+        private static IContainer _container;
+
+        public static void RegisterDependencies()
         {
+            var builder = new ContainerBuilder();
+
+            //Views
+            builder.RegisterType<DataBreachPage>().SingleInstance();
+
+            //ViewModels
+            builder.RegisterType<DataBreachViewModel>().SingleInstance();
+
+            //Services
+            //builder.RegisterType<DataBreachService>().As<id
+
+            _container = builder.Build();
+        }
+
+        public static object Resolve(Type typeName)
+        {
+            return _container.Resolve(typeName);
+        }
+
+        public static T Resolve<T>()
+        {
+            return _container.Resolve<T>();
         }
     }
 }
