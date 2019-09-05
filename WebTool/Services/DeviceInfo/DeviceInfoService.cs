66766cf94ef820c1026172608e40d1fdf39536fe
Andrés Balderas <balnorie@gmail.com>
2019-09-04 22:25:37 -0500

    Create DeviceInfo Service

---
+++ WebTool/Services/DeviceInfo/DeviceInfoService.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebTool.Services.DeviceInfo
+{
+    public class DeviceInfoService
+    {
+        private HttpClient _httpClient = new HttpClient();
+
+        public async Task<string> GetCurrentIPAsync()
+        {
+            var output = "";
+
+            var ip = await _httpClient.GetStringAsync("https://api.ipify.org");
+            output = $"Public IP address: {ip}";
+
+            return output;
+        }
+    }
+}
