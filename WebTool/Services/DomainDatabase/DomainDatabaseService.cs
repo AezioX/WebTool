51d80145c27a36d6a8594760f316ee42b7ee7d40
Andrés Balderas <balnorie@gmail.com>
2019-09-04 23:26:44 -0500

    Create DomainDatabase Service

---
+++ WebTool/Services/DomainDatabase/DomainDatabaseService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebTool.Models.DomainDatabase;
+
+namespace WebTool.Services.DomainDatabase
+{
+    public class DomainDatabaseService
+    {
+        private HttpClient _httpClient = new HttpClient();
+
+        private const string baseURL = "https://api.domainsdb.info/search?query=";
+
+        public async Task<ObservableCollection<DomainsData>> CheckDomainsDatabase(string data)
+        {
+            var output = new ObservableCollection<DomainsData>();
+
+            var jsonString = await _httpClient.GetStringAsync($"{baseURL}{data}");
+
+            var domains = JsonConvert.DeserializeObject<JsonDomainsData>(jsonString);
+
+            foreach (var domain in domains.Domains)
+            {
+                output.Add(new DomainsData
+                {
+                    Domain = domain.DomainDomain,
+                    Create_date = domain.CreateDate,
+                    Expiry_date = domain.ExpiryDate,
+                    Update_date = domain.UpdateDate,
+                    IsDead = domain.IsDead,
+                    NameServers = domain.Ns
+                });
+            }
+
+            return output;
+        }
+    }
+}
