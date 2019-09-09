706819be19b96fbfc03b705386572841d3958a80
Andrés Balderas <balnorie@gmail.com>
2019-09-09 12:08:59 -0500

    Update ServerMonitor ViewModel

--- WebTool/ViewModels/ServerMonitorViewModel.cs
+++ WebTool/ViewModels/ServerMonitorViewModel.cs
@@ -1,11 +1,51 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace WebTool.ViewModels
 {
     public class ServerMonitorViewModel : BaseViewModel
     {
+        public ICommand RefreshCommand => new Command(Refresh);
+
         public ServerMonitorViewModel()
         {
+        }
+
+        private async void Refresh()
+        {
+            IsBusy = true;
+
+            try
+            {
+                ServersData = await _monitor.GetUpdatedServersData();
+            }
+            catch (Exception ex)
+            {
+                IsBusy = false;
+                await Application.Current.MainPage.DisplayAlert("Error", $"{ex.Message}", "OK");
+            }
+
+            LastUpdated = "Last updated: Aug 10, 15:15";
+
+            IsBusy = false;
+        }
+
+        private ObservableCollection<Server> _serversData;
+        public ObservableCollection<Server> ServersData
+        {
+            get => _serversData;
+
+            set => SetProperty(ref _serversData, value);
+        }
+
+        private string _lastUpdated;
+        public string LastUpdated
+        {
+            get => _lastUpdated;
+
+            set => SetProperty(ref _lastUpdated, value);
         }
     }
 }
