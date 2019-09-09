a165ced2d9e38ab3c46edfa789fdbb1673b75263
Andrés Balderas <balnorie@gmail.com>
2019-09-09 12:09:52 -0500

    Create Server Model

---
+++ WebTool/Models/ServerMonitor/Server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebTool.Models.ServerMonitor
+{
+    public class Server
+    {
+        public string Name { get; set; } = "";
+        public string IP { get; set; } = "";
+        public string HostName { get; set; } = "";
+        public string DisplayHost { get; set; } = "";
+        public string Status { get; set; } = "";
+        public List<string> Logs { get; set; } = new List<string>();
+    }
+}
