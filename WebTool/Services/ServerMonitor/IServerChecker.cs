c537c0f962c37b5390410ff227484981e59dc4b9
Andrés Balderas <balnorie@gmail.com>
2019-09-09 12:26:12 -0500

    Create ServerChecker and extract interface

---
+++ WebTool/Services/ServerMonitor/IServerChecker.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using WebTool.Models.ServerMonitor;
+
+namespace WebTool.Services.ServerMonitor
+{
+    public interface IServerChecker
+    {
+        Task<ObservableCollection<Server>> Check(ObservableCollection<Server> servers);
+    }
+}
---
+++ WebTool/Services/ServerMonitor/ServerChecker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebTool.Models.ServerMonitor;
+
+namespace WebTool.Services.ServerMonitor
+{
+    public class ServerChecker : IServerChecker
+    {
+        private HttpClient _httpClient;
+
+        public ServerChecker()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<ObservableCollection<Server>> Check(ObservableCollection<Server> servers)
+        {
+            var output = new ObservableCollection<Server>();
+
+            await Task.Run(() =>
+            {
+                Parallel.ForEach(servers, async (server) =>
+                {
+                    try
+                    {
+                        using (HttpResponseMessage response = await _httpClient.GetAsync(server.DisplayHost))
+                        {
+                            string serverStatus = "";
+                            serverStatus = Convert.ToString(response.StatusCode);
+
+                            output.Add(new Server
+                            {
+                                Name = $"{server.Name}",
+                                IP = $"{server.IP}",
+                                HostName = $"{server.HostName}",
+                                DisplayHost = $"{server.DisplayHost}",
+                                Status = $"{serverStatus}"
+                            });
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        output.Add(new Server
+                        {
+                            Name = $"{server.Name}",
+                            IP = $"{server.IP}",
+                            HostName = $"{server.HostName}",
+                            DisplayHost = $"{ex.Message}",
+                            Status = $""
+                        });
+                    }
+                });
+            });
+
+            return output;
+        }
+    }
+}
