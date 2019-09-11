fc74bd31b433f0a8862b00ed0c6de26b6feb5b76
Andrés Balderas <balnorie@gmail.com>
2019-09-11 17:49:15 -0500

    Create ServerListAdd View and ViewModel

---
+++ WebTool/ViewModels/ServerListAddViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebTool.ViewModels
+{
+    public class ServerListAddViewModel : BaseViewModel
+    {
+        public ServerListAddViewModel()
+        {
+        }
+    }
+}
---
+++ WebTool/Views/ServerListAddPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using WebTool.ViewModels;
+using Xamarin.Forms;
+
+namespace WebTool.Views
+{
+    public partial class ServerListAddPage : ContentPage
+    {
+        public ServerListAddPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new ServerListAddViewModel();
+        }
+    }
+}
