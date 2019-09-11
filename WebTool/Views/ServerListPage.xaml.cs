d03b8caeac843567d193a322fdae1efb1f474611
Andrés Balderas <balnorie@gmail.com>
2019-09-11 13:14:15 -0500

    Create ServerList Page

---
+++ WebTool/Views/ServerListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace WebTool.Views
+{
+    public partial class ServerListPage : ContentPage
+    {
+        public ServerListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
