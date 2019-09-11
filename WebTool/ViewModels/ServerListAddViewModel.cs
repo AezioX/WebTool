52f22faab3ba8bc82c1b010fd27069d6a60b0f21
Andrés Balderas <balnorie@gmail.com>
2019-09-11 18:50:14 -0500

    Update ServerListAdd ViewModel

--- WebTool/ViewModels/ServerListAddViewModel.cs
+++ WebTool/ViewModels/ServerListAddViewModel.cs
@@ -1,11 +1,36 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace WebTool.ViewModels
 {
     public class ServerListAddViewModel : BaseViewModel
     {
+        public ICommand AddCommand => new Command(Add);
+
         public ServerListAddViewModel()
         {
+        }
+
+        private void Add()
+        {
+            
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+
+            set => SetProperty(ref _name, value);
+        }
+
+        private string _hostName;
+        public string HostName
+        {
+            get => _hostName;
+
+            set => SetProperty(ref _hostName, value);
         }
     }
 }
