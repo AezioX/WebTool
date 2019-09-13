d51b0a0569fcc30f65c1b171ba5492663d74d69a
Andrés Balderas <balnorie@gmail.com>
2019-09-13 17:43:21 -0500

    Add Home Page to Shell

--- WebTool/ViewModels/HomeViewModel.cs
+++ WebTool/ViewModels/HomeViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 namespace WebTool.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
+        public ICommand GoToServerMonitorCommand => new Command(GoToServerMonitor);
+
         public HomeViewModel()
         {
+            
+        }
 
+        private async void GoToServerMonitor()
+        {
+            await Shell.Current.GoToAsync("//ServerMonitor");
         }
     }
 }
