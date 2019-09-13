c13d1bea37df1884f5aa81262b871c2fc0ed9a99
Andrés Balderas <balnorie@gmail.com>
2019-09-13 18:11:51 -0500

    Create base code for Home ViewModel

--- WebTool/ViewModels/HomeViewModel.cs
+++ WebTool/ViewModels/HomeViewModel.cs
@@ -6,16 +6,19 @@ namespace WebTool.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
-        public ICommand GoToServerMonitorCommand => new Command(GoToServerMonitor);
+        public ICommand GoToServerMonitorCommand =>
+            new Command(async() => await Shell.Current.GoToAsync("//ServerMonitor"));
 
-        public HomeViewModel()
-        {
-            
-        }
+        public ICommand GoToDomainDatabaseCommand =>
+            new Command(async () => await Shell.Current.GoToAsync("//DomainDatabase"));
 
-        private async void GoToServerMonitor()
-        {
-            await Shell.Current.GoToAsync("//ServerMonitor");
-        }
+        public ICommand GoToDataBreachCheckerCommand =>
+            new Command(async () => await Shell.Current.GoToAsync("//DataBreachChecker"));
+
+        public ICommand GoToDeviceInfoCommand =>
+            new Command(async () => await Shell.Current.GoToAsync("//DeviceInfo"));
+
+        public ICommand GoToSettingsCommand =>
+            new Command(async () => await Shell.Current.GoToAsync("//Settings"));
     }
 }
