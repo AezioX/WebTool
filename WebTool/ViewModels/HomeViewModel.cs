1e8d5e15e8f023c68643d90f25c967dc38a58a2a
Andrés Balderas <balnorie@gmail.com>
2019-09-13 17:22:49 -0500

    Create Home Page and ViewModel

---
+++ WebTool/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebTool.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public HomeViewModel()
+        {
+        }
+    }
+}
---
+++ WebTool/Views/HomePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using WebTool.ViewModels;
+using Xamarin.Forms;
+
+namespace WebTool.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            BindingContext = new HomeViewModel();
+        }
+    }
+}
