f4602c4ca41d654a38944cdf188653b0d7b9ef1a
Andrés Balderas <balnorie@gmail.com>
2019-09-13 21:11:41 -0500

    Use toolbar item instead of button for refresh in ServerMonitor page

--- WebTool.Android/Resources/Resource.designer.cs
+++ WebTool.Android/Resources/Resource.designer.cs
@@ -9085,28 +9085,34 @@ namespace WebTool.Droid
 			public const int plusred = 2131165512;
 			
 			// aapt resource value: 0x7F070149
-			public const int settingsgreen = 2131165513;
+			public const int refreshgreen = 2131165513;
 			
 			// aapt resource value: 0x7F07014A
-			public const int settingsred = 2131165514;
+			public const int refreshred = 2131165514;
 			
 			// aapt resource value: 0x7F07014B
-			public const int snakegreen = 2131165515;
+			public const int settingsgreen = 2131165515;
 			
 			// aapt resource value: 0x7F07014C
-			public const int snakered = 2131165516;
+			public const int settingsred = 2131165516;
 			
 			// aapt resource value: 0x7F07014D
-			public const int tigergreen = 2131165517;
+			public const int snakegreen = 2131165517;
 			
 			// aapt resource value: 0x7F07014E
-			public const int tigerred = 2131165518;
+			public const int snakered = 2131165518;
 			
 			// aapt resource value: 0x7F07014F
-			public const int tooltip_frame_dark = 2131165519;
+			public const int tigergreen = 2131165519;
 			
 			// aapt resource value: 0x7F070150
-			public const int tooltip_frame_light = 2131165520;
+			public const int tigerred = 2131165520;
+			
+			// aapt resource value: 0x7F070151
+			public const int tooltip_frame_dark = 2131165521;
+			
+			// aapt resource value: 0x7F070152
+			public const int tooltip_frame_light = 2131165522;
 			
 			static Drawable()
 			{
