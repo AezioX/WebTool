1971765449ad2a138015c1f55ed7264feb392ada
Andrés Balderas <balnorie@gmail.com>
2019-09-23 20:27:03 -0500

    Add hyperlink to HIBP website on DataBreachInfo page

--- WebTool/ViewModels/DataBreachInfoViewModel.cs
+++ WebTool/ViewModels/DataBreachInfoViewModel.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace WebTool.ViewModels
 {
     public class DataBreachInfoViewModel : BaseViewModel
     {
-        public DataBreachInfoViewModel()
+        public ICommand GoToHIBPCommand => new Command<string>(OpenBrowser);
+
+        private void OpenBrowser(string url)
         {
+            Device.OpenUri(new Uri(url));
         }
     }
 }
