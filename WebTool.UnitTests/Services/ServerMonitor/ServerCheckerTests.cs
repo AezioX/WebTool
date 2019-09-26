e794f8af80310c3e5df2b75e0eebd76ca5c271f7
Andrés Balderas <balnorie@gmail.com>
2019-09-25 22:25:21 -0500

    Write Unit Tests for ServerChecker

---
+++ WebTool.UnitTests/Services/ServerMonitor/ServerCheckerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebTool.Services.ServerMonitor;
+
+namespace WebTool.UnitTests.Services.ServerMonitor
+{
+    [TestFixture]
+    public class ServerCheckerTests
+    {
+        [Test]
+        [TestCase("https://www.google.com")]
+        [TestCase("https://google.com")]
+        //[TestCase("thisisaninvaliddomain")]
+        public async Task CheckIfDomainIsValidAsync_DomainIsValid_ReturnTrue(string domain)
+        {
+            var serverChecker = new ServerChecker();
+
+            var result = await serverChecker.CheckIfDomainIsValidAsync(domain);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        [TestCase("https://google")]
+        [TestCase("thisisaninvaliddomain")]
+        public async Task CheckIfDomainIsValidAsync_DomainIsNotValid_ReturnFalse(string domain)
+        {
+            var serverChecker = new ServerChecker();
+
+            var result = await serverChecker.CheckIfDomainIsValidAsync(domain);
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
