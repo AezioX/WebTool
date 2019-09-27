f3c2e9e4ccf6e70f61d2eca8aa471684c389b411
Andrés Balderas <balnorie@gmail.com>
2019-09-26 20:05:33 -0500

    Write Unit Test for ServerMonitorService

--- WebTool.UnitTests/Services/ServerMonitor/ServerMonitorServiceTests.cs
+++ WebTool.UnitTests/Services/ServerMonitor/ServerMonitorServiceTests.cs
@@ -4,6 +4,7 @@ using WebTool.Services.ServerMonitor;
 using Moq;
 using WebTool.Models.ServerMonitor;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace WebTool.UnitTests.Services.ServerMonitor
 {
@@ -39,6 +40,22 @@ namespace WebTool.UnitTests.Services.ServerMonitor
             var result = await serverMonitorService.GetUpdatedServersDataAsync();
 
             Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public async Task GetUpdatedServersData_WhenServerListIsNotEmpty_ReturnListOfServersObject()
+        {
+            //Set up ServersService mock object
+            var serversService = new Mock<IServersService>();
+            var servers = new Servers();
+            servers.MonitoredServers.Add(new Server { Name = "Test" });
+            serversService.Setup(sS => sS.GetStoredServerListAsync()).ReturnsAsync(servers);
+
+            var serverMonitorService = new ServerMonitorService(new ServerChecker(), serversService.Object);
+
+            var result = await serverMonitorService.GetUpdatedServersDataAsync();
+
+            Assert.That(result, Is.TypeOf<ObservableCollection<Server>>());
         }
     }
 }
