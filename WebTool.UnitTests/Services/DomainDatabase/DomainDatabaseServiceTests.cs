dd44833b81327f8e219ff8b84d73eb71194cc751
Andrés Balderas <balnorie@gmail.com>
2019-09-26 20:11:36 -0500

    Write Unit Test for DomainDatabaseService

--- WebTool.UnitTests/Services/DomainDatabase/DomainDatabaseServiceTests.cs
+++ WebTool.UnitTests/Services/DomainDatabase/DomainDatabaseServiceTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using WebTool.Services.DomainDatabase;
+using WebTool.Models.DomainDatabase;
 
 namespace WebTool.UnitTests.Services.DomainDatabase
 {
@@ -18,6 +20,17 @@ namespace WebTool.UnitTests.Services.DomainDatabase
             var result = results[0].Domain;
 
             Assert.That(result, Does.Contain("test"));
+        }
+
+        [Test]
+        [TestCase("test")]
+        public async Task CheckDomainsDatabase_ValidQueryString_ReturnListOfDomainsDataObject(string data)
+        {
+            var domainDatabaseService = new DomainDatabaseService();
+
+            var results = await domainDatabaseService.CheckDomainsDatabase(data);
+
+            Assert.That(results, Is.TypeOf<ObservableCollection<DomainsData>>());
         }
     }
 }
