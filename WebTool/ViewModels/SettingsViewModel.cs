660478d54d7aed30515d0906d1e0fd95a58a2de4
Andrés Balderas <balnorie@gmail.com>
2019-09-27 23:55:43 -0500

    Ask for confirmation when trying to restore the dafault server list

--- WebTool/ViewModels/SettingsViewModel.cs
+++ WebTool/ViewModels/SettingsViewModel.cs
@@ -46,72 +46,77 @@ namespace WebTool.ViewModels
 
         private async void RestoreServerList()
         {
-            try
+            var answer = await Application.Current.MainPage.DisplayAlert("", "Are you sure you want to restore the default server list?", "YES", "NO");
+
+            if (answer == true)
             {
-                var servers = new Servers();
-
-                servers.MonitoredServers.Add(new Server
+                try
                 {
-                    ID = Guid.NewGuid().ToString(),
-                    Name = "Google",
-                    Status = "",
-                    HostName = "https://google.com",
-                    Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
-                });
+                    var servers = new Servers();
 
-                servers.MonitoredServers.Add(new Server
+                    servers.MonitoredServers.Add(new Server
+                    {
+                        ID = Guid.NewGuid().ToString(),
+                        Name = "Google",
+                        Status = "",
+                        HostName = "https://google.com",
+                        Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
+                    });
+
+                    servers.MonitoredServers.Add(new Server
+                    {
+                        ID = Guid.NewGuid().ToString(),
+                        Name = "Twitter",
+                        Status = "",
+                        HostName = "https://twitter.com",
+                        Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
+                    });
+
+
+                    servers.MonitoredServers.Add(new Server
+                    {
+                        ID = Guid.NewGuid().ToString(),
+                        Name = "Amazon",
+                        Status = "",
+                        HostName = "https://amazon.com",
+                        Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
+                    });
+
+                    servers.MonitoredServers.Add(new Server
+                    {
+                        ID = Guid.NewGuid().ToString(),
+                        Name = "YouTube",
+                        Status = "",
+                        HostName = "https://youtube.com",
+                        Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
+                    });
+
+                    servers.MonitoredServers.Add(new Server
+                    {
+                        ID = Guid.NewGuid().ToString(),
+                        Name = "Apple",
+                        Status = "",
+                        HostName = "https://apple.com",
+                        Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
+                    });
+
+                    servers.MonitoredServers.Add(new Server
+                    {
+                        ID = Guid.NewGuid().ToString(),
+                        Name = "Netflix",
+                        Status = "",
+                        HostName = "https://netflix.com",
+                        Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
+                    });
+
+                    await BlobCache.UserAccount.InsertObject("Servers", servers);
+
+                    await Application.Current.MainPage.DisplayAlert("", "Default server list restored.", "OK");
+                }
+                catch (Exception ex)
                 {
-                    ID = Guid.NewGuid().ToString(),
-                    Name = "Twitter",
-                    Status = "",
-                    HostName = "https://twitter.com",
-                    Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
-                });
-
-
-                servers.MonitoredServers.Add(new Server
-                {
-                    ID = Guid.NewGuid().ToString(),
-                    Name = "Amazon",
-                    Status = "",
-                    HostName = "https://amazon.com",
-                    Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
-                });
-
-                servers.MonitoredServers.Add(new Server
-                {
-                    ID = Guid.NewGuid().ToString(),
-                    Name = "YouTube",
-                    Status = "",
-                    HostName = "https://youtube.com",
-                    Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
-                });
-
-                servers.MonitoredServers.Add(new Server
-                {
-                    ID = Guid.NewGuid().ToString(),
-                    Name = "Apple",
-                    Status = "",
-                    HostName = "https://apple.com",
-                    Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
-                });
-
-                servers.MonitoredServers.Add(new Server
-                {
-                    ID = Guid.NewGuid().ToString(),
-                    Name = "Netflix",
-                    Status = "",
-                    HostName = "https://netflix.com",
-                    Logs = new List<string> { $"Added to list: {DateTime.Now.ToString("yyyy-MM-dd  hh:mm:ss tt")}" }
-                });
-
-                await BlobCache.UserAccount.InsertObject("Servers", servers);
-
-                await Application.Current.MainPage.DisplayAlert("", "Default server list restored.", "OK");
-            }
-            catch (Exception ex)
-            {
-                await Application.Current.MainPage.DisplayAlert("Error", $"{ex.Message}", "OK");
+                    await Application.Current.MainPage.DisplayAlert("Error", $"{ex.Message}", "OK");
+                }
             }
         }
 
