b01d47e826a09d6996c7a41bf0484d442849ef8d
Andrés Balderas <balnorie@gmail.com>
2019-09-30 21:10:03 -0500

    Create DomainDatabaseInfo Page and ViewModel

---
+++ WebTool/ViewModels/DomainDatabaseInfoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebTool.ViewModels
+{
+    public class DomainDatabaseInfoViewModel : BaseViewModel
+    {
+        public DomainDatabaseInfoViewModel()
+        {
+        }
+    }
+}
---
+++ WebTool/Views/DomainDatabaseInfoPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using WebTool.ViewModels;
+using Xamarin.Forms;
+
+namespace WebTool.Views
+{
+    public partial class DomainDatabaseInfoPage : ContentPage
+    {
+        public DomainDatabaseInfoPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new DomainDatabaseInfoViewModel();
+        }
+    }
+}
